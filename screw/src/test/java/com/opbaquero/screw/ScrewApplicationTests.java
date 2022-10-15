8e26e40a070a6a48e9691497a8bb4b2e3089ff4c
oscarpon <oscar.pontebaquero@gmail.com>
2022-10-15 08:01:56 +0200

    First commit

---
+++ screw/src/test/java/com/opbaquero/screw/ScrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opbaquero.screw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
